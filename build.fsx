c2a4fbd8775dafbb4fa1d0df7d01788389b28d49
Chet Husk <chusk3@gmail.com>
2019-01-30 14:27:24 -0500

    enable projectreload tests

--- build.fsx
+++ build.fsx
@@ -105,7 +105,6 @@ let isTestSkipped cfg (fn: string) =
   | AnyNetcoreRuntime, _, "NoFSharpCoreReference", "Runner.fsx" ->
     Some "know failure, the FSharp.Core is not added if not in the fsc args list"
   // fsproj in test suite use ToolsVersion 4 (VS2010) and is not always installed
-  | _, _, "ProjectReload", "Runner.fsx"
   | _, _, "RobustCommands", "CompleteBadPositionRunner.fsx"
   | _, _, "RobustCommands", "CompleteNoSuchFileRunner.fsx"
   | _, _, "RobustCommands", "ParseNoSuchFileRunner.fsx"
